495f7b0ddeff16081c3b986582f028e06f6eaf41
Administrator <admin@example.com>
2020-12-02 18:46:20 +0800

    first commit

---
+++ src/main/java/org/spring/springboot/service/CityService.java
@@ -0,0 +1,44 @@
+package org.spring.springboot.service;
+
+import org.spring.springboot.domain.City;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 城市业务逻辑接口类
+ *
+ */
+public interface CityService {
+    /**
+     * 根据城市 ID,查询城市信息
+     *
+     * @param id
+     * @return
+     */
+    City findCityById(Long id) throws IOException;
+
+    /**
+     * 新增城市信息
+     *
+     * @param city
+     * @return
+     */
+    City saveCity(City city);
+
+    /**
+     * 更新城市信息
+     *
+     * @param city
+     * @return
+     */
+    Long updateCity(City city);
+
+    /**
+     * 根据城市 ID,删除城市信息
+     *
+     * @param id
+     * @return
+     */
+    Long deleteCity(Long id);
+}
